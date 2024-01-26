4d800f2181269a99093a39b20895c419f0b8d7d6
dusty.shaw <dusty.shaw@ENGRJJCP5S3>
2024-01-25 20:15:19 -0700

    commiting

--- ToDoTests/UnitTest1.cs
+++ ToDoTests/UnitTest1.cs
@@ -1,7 +1,22 @@
+using Moq;
+using RazorClassLibrary.Data;
+using RazorClassLibrary.Services;
+using ToDoMauiApp;
+using ToDoMauiApp.Services;
+
 namespace ToDoTests;
 
 public class UnitTest1
 {
+    private readonly AppService _sut;
+    private readonly Mock<ToDoRepository> _todoRepoMock = new Mock<ToDoRepository>();
+    //private readonly Mock<IService> _logger = new Mock<IService>();
+
+    public UnitTest1()
+    {
+        _sut = new AppService();
+    }
+
     [Fact]
     public void PassingTest()
     {
@@ -15,12 +30,26 @@ public class UnitTest1
     }
 
     [Fact]
-    public void UpdatesOnClientShowUpOnServer()
+    public async Task GetSomeTodo()
+    {
+        // Arrange
+        var todos = await _sut.GetAllTodos();
+
+        // Act
+        bool greaterThanZero = todos.Count > 0;
+
+        // Assert
+        Assert.True(greaterThanZero);
+    }
+
+    [Fact]
+    public async void UpdatesOnClientShowUpOnServer()
     {
         // Arrange
         
 
         // Act
+        var allTodos = await _sut.GetAllTodos();
 
         // Assert
     }
